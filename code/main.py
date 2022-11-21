a253c742dfb5b71ddab91cbf35a4b204f6f25ddb
Paul Thomas <paulthomas2107@gmail.com>
2022-11-21 19:40:13 +0000

    Complete

--- code/main.py
+++ code/main.py
@@ -168,6 +168,12 @@ class Game:
         score_rect = score_surf.get_rect(topleft=(10, -10))
         screen.blit(score_surf, score_rect)
 
+    def victory_message(self):
+        if not self.aliens.sprites():
+            victory_surf = self.font.render('You won !', False, 'white')
+            victory_rectangle = victory_surf.get_rect(center=(screen_width / 2, screen_height / 2))
+            screen.blit(victory_surf, victory_rectangle)
+
     def run(self):
         self.player.update()
         self.alien_lasers.update()
@@ -186,6 +192,7 @@ class Game:
         self.extra.draw(screen)
         self.display_lives()
         self.display_score()
+        self.victory_message()
 
 
 if __name__ == '__main__':
