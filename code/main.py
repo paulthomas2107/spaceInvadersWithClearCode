c4c4cae048c12c26c4b111f6546bf3ceb63fe279
Paul Thomas <paulthomas2107@gmail.com>
2022-11-21 19:23:33 +0000

    More...

--- code/main.py
+++ code/main.py
@@ -12,8 +12,16 @@ class CRT:
         self.tv = pygame.image.load('../graphics/tv.png').convert_alpha()
         self.tv = pygame.transform.scale(self.tv, (screen_width, screen_height))
 
+    def create_crt_lines(self):
+        line_height = 3
+        line_amount = int(screen_height / line_height)
+        for line in range(line_amount):
+            y_pos = line * line_height
+            pygame.draw.line(self.tv, 'black', (0, y_pos), (screen_width, y_pos), 1)
+
     def draw(self):
         self.tv.set_alpha(randint(75, 90))
+        self.create_crt_lines()
         screen.blit(self.tv, (0, 0))
 
 
