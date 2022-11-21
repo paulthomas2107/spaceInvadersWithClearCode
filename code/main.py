4cf584cbe9837746363f979d176ea4c211bfda7a
Paul Thomas <paulthomas2107@gmail.com>
2022-11-21 16:54:42 +0000

    More...

--- code/main.py
+++ code/main.py
@@ -22,6 +22,7 @@ class Game:
         # Alien set up
         self.aliens = pygame.sprite.Group()
         self.alien_setup(rows=6, cols=8)
+        self.alien_direction = 1
 
     def create_obstacle(self, x_start, y_start, offset_x):
         for row_index, row in enumerate(self.shape):
@@ -36,16 +37,31 @@ class Game:
         for offset_x in offset:
             self.create_obstacle(x_start, y_start, offset_x)
 
-    def alien_setup(self, rows, cols, x_distance=60, y_distance=48):
+    def alien_setup(self, rows, cols, x_distance=60, y_distance=48, x_offset=70, y_offset=100):
         for row_index, row in enumerate(range(rows)):
             for col_index, col in enumerate(range(cols)):
-                x = col_index * x_distance
-                y = row_index * y_distance
-                alien_sprite = Alien('red', x, y)
+                x = col_index * x_distance + x_offset
+                y = row_index * y_distance + y_offset
+                if row_index == 0:
+                    alien_sprite = Alien('yellow', x, y)
+                elif 1 <= row_index <= 2:
+                    alien_sprite = Alien('green', x, y)
+                else:
+                    alien_sprite = Alien('red', x, y)
                 self.aliens.add(alien_sprite)
+
+    def alien_position_checker(self):
+        all_aliens = self.aliens.sprites()
+        for alien in all_aliens:
+            if alien.rect.right >= screen_width:
+                self.alien_direction = -1
+            elif alien.rect.left <= 0:
+                self.alien_direction = 1
 
     def run(self):
         self.player.update()
+        self.aliens.update(self.alien_direction)
+        self.alien_position_checker()
         self.player.sprite.lasers.draw(screen)
         self.player.draw(screen)
         self.blocks.draw(screen)
