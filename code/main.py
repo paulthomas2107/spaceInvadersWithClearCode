cef5dbe86c0874a70f9de9b7807691808153161e
Paul Thomas <paulthomas2107@gmail.com>
2022-11-21 16:06:08 +0000

    More...

--- code/main.py
+++ code/main.py
@@ -14,16 +14,20 @@ class Game:
         self.shape = obstacle.shape
         self.block_size = 6
         self.blocks = pygame.sprite.Group()
-        self.create_obstacle()
+        self.create_multiple_obstacles(0, 480, 0, 100, 200)
 
-    def create_obstacle(self):
+    def create_obstacle(self, x_start, y_start, offset_x):
         for row_index, row in enumerate(self.shape):
             for col_index, col in enumerate(row):
                 if col == 'x':
-                    x = col_index * self.block_size
-                    y = row_index * self.block_size
+                    x = x_start + col_index * self.block_size + offset_x
+                    y = y_start + row_index * self.block_size
                     block = obstacle.Block(self.block_size, (241, 79, 80), x, y)
                     self.blocks.add(block)
+
+    def create_multiple_obstacles(self, x_start, y_start, *offset):
+        for offset_x in offset:
+            self.create_obstacle(x_start, y_start, offset_x)
 
     def run(self):
         self.player.update()
