543c1031ebcf8d81026606f0c27710f86cbc5f92
Paul Thomas <paulthomas2107@gmail.com>
2022-11-21 18:27:52 +0000

    More...

--- code/main.py
+++ code/main.py
@@ -84,6 +84,35 @@ class Game:
             self.extra.add(Extra(choice(['right', 'left']), screen_width))
             self.extra_spawn_time = randint(400, 800)
 
+    def collision_checks(self):
+        # player lasers
+        if self.player.sprite.lasers:
+            for laser in self.player.sprite.lasers:
+                # obstacle collision
+                if pygame.sprite.spritecollide(laser, self.blocks, True):
+                    laser.kill()
+                # alien collisions
+                if pygame.sprite.spritecollide(laser, self.aliens, True):
+                    laser.kill()
+                # extra collision
+                if pygame.sprite.spritecollide(laser, self.extra, True):
+                    laser.kill()
+        # alien lasers
+        if self.alien_lasers:
+            for laser in self.alien_lasers:
+                if pygame.sprite.spritecollide(laser, self.blocks, True):
+                    laser.kill()
+                if pygame.sprite.spritecollide(laser, self.player, False):
+                    laser.kill()
+
+        # aliens
+        if self.aliens:
+            for alien in self.aliens:
+                pygame.sprite.spritecollide(alien, self.blocks, True)
+                if pygame.sprite.spritecollide(alien, self.player, False):
+                    pygame.quit()
+                    sys.exit()
+
     def run(self):
         self.player.update()
 
@@ -93,6 +122,7 @@ class Game:
 
         self.extra_alien_timer()
         self.extra.update()
+        self.collision_checks()
 
         self.player.sprite.lasers.draw(screen)
         self.player.draw(screen)
